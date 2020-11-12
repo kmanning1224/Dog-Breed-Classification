e7c2f8a1eac8a2ae8b15a790c7ab8def8b45be56
SAS 826 <sasotillo826@gmail.com>
2020-11-12 17:50:29 -0500

    Bar Plot works

--- static/js/plotsas.js
+++ static/js/plotsas.js
@@ -1,5 +1,5 @@
 function PlotDog() {
-let queryUrl = 'http://127.0.0.1:5000/plotfunc'
+let queryUrl = '/plotfunc'
 
 d3.json(queryUrl,function(data){
     let bar = d3.select('#barplot');
@@ -15,10 +15,60 @@ d3.json(queryUrl,function(data){
     let p4 = [];
 
     breed1.push(data[0].Breed);
-    console.log(breed1)    
+    breed2.push(data[1].Breed);
+    breed3.push(data[2].Breed);
+    breed4.push(data[3].Breed);
+    console.log(breed3);
 
+    p1.push(data[0].Percentage);
+    p2.push(data[1].Percentage);
+    p3.push(data[2].Percentage);
+    p4.push(data[3].Percentage);
+    console.log(p1);  
+    
+    let one = Math.round(p1[0] * 100).toFixed(2);
+    let two = Math.round(p2[0] * 100).toFixed(2);
+    let three = Math.round(p3[0] * 100).toFixed(2);
+    let four = Math.round(p4[0] * 100).toFixed(2);
+    console.log(two);
 
+    let b1 = breed1[0].replace('_', ' ');
+    let b2 = breed2[0].replace('_', ' ');
+    let b3 = breed3[0].replace('_', ' ');
+    let b4 = breed4[0].replace('_', ' ');
+    console.log(b3);
 
+    // var yValue = [one, two, three, four];
+
+    var data = [
+        {
+          x: [b1, b2, b3, b4],
+          y: [one, two, three, four],
+          type: 'bar',
+          marker: {
+            color: ['#2171b5', '#6baed6', '#bdd7e7', '#eff3ff'],
+            },
+        }],
+
+    var layout = {
+          title: `Probability of Dog Breeds`,
+          font: {
+                size: 12
+              },
+          showlegend: false,
+          xaxis: {
+                tickangle: -45
+              },
+          yaxis: {
+                zeroline: false,
+                gridwidth: 2,
+              },
+          height:600,
+          width: 600,
+            }
+          
+
+    Plotly.newPlot('barplot', data, layout);
 
     }); 
 
