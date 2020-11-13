6f1b0c5e495f8a4029b44a5f522ced9abbb1101e
Kelsey Wyatt <kwyattkkg@gmail.com>
2020-11-13 00:57:55 -0500

    created final.html need to work on css and predict button

--- Kelsey/app.js
+++ Kelsey/app.js
@@ -1,12 +1,36 @@
 var loader = function(e) {
     let file = e.target.files; 
-
-    let show = "<span>Selected image: </span>" + file[0]
-
     let output = document.getElementById("selector");
-    output.innerHTML = show; 
-    output.classList.add("active");
-}
+
+    if (file[0].type.match("image")) {
+        //file is image
+        let reader = new FileReader();
+
+        reader.addEventListener("load", function(e) {
+            let data = e.target.result;
+            let image = document.createElement("img");
+            image.src = data; 
+
+            output.innerHTML = "";
+            output.insertBefore(image, null);
+            output.classList.add("image");
+        });
+
+        reader.readAsDataURL(file[0]);
+    } else {
+        //file is not image
+        let show = "<span>Selected File: </span>";
+        show = show + file[0].name; 
+
+        output.innerHTML = show; 
+        output.classList.add("active");
+
+        if (output.classList.contains("image")) {
+            output.classList.remove("image");
+        }
+    }
+   
+};
 
 //add event listener for input 
 
